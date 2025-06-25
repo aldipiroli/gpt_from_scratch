00bdf4664764f4fa61d061239314b3442fbb9245
Aldi Piroli <aldipiroli@gmail.com>
2025-06-25 17:36:32 +0200

    fixed bigram model test

--- gpt/test/test_model.py
+++ gpt/test/test_model.py
@@ -4,7 +4,9 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import tempfile
 
+import numpy as np
 import torch
+from dataset.tokenizer import WordLevelTokenizer
 from model.bigram_model import BigramModel
 from model.gpt_model import GPTModel
 from utils.misc import get_logger, load_config
@@ -30,14 +32,19 @@ def test_bigram_model():
 
 
 def test_bigram_model_generation():
-    cfg_model = cfg["MODEL"]
-    cfg["DATA"]["vocab_size"] = 65
+    x = "hello, world! This is the next"
+    vocab = sorted(list(set(x)))
+    cfg["DATA"]["vocab_size"] = len(vocab)
+
+    tok = WordLevelTokenizer(vocab)
     model = BigramModel(cfg)
-    B, T = 2, cfg_model["context_len"]
-    x = torch.randint(0, cfg["MODEL"]["context_len"], (B, T))
+    B = 1
+    x_enc = tok.encode(x)
+    x_enc = torch.tensor(np.array(x_enc))
+    x_enc = x_enc.reshape(B, -1)  # B,T
     max_tokens = 10
-    out = model.generate(x, max_tokens=max_tokens)
-    assert out.shape == (B, max_tokens)
+    out = model.generate(x_enc, max_tokens=max_tokens)
+    assert out.shape == (B, x_enc.shape[1] + max_tokens)
 
 
 if __name__ == "__main__":
