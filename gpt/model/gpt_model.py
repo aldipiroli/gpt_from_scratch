a822d3e1f009b0e312005e55d82095a06bd4c94f
Aldi Piroli <aldipiroli@gmail.com>
2025-06-25 18:01:33 +0200

    Added attention module

--- gpt/model/gpt_model.py
+++ gpt/model/gpt_model.py
@@ -1,12 +1,54 @@
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Attention(nn.Module):
+    def __init__(self, embed_size):
+        super().__init__()
+        self.embed_size = embed_size
+        self.keys = nn.Linear(embed_size, embed_size)
+        self.query = nn.Linear(embed_size, embed_size)
+        self.values = nn.Linear(embed_size, embed_size)
+
+    def forward(self, x):
+        k = self.keys(x)
+        q = self.query(x)
+        v = self.values(x)  # B,T,C
+
+        qk = q @ k.transpose(2, 1)  # B, C, C
+        attention = F.softmax(qk / self.embed_size**-0.5, -1) @ v  # B, T, C
+        return attention
+
+
+class TransformerLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
 
 
 class GPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.cfg = cfg
-        self.ffw = nn.Linear(10, 1)
+        self.embed_size = cfg["MODEL"]["embed_size"]
+        self.vocab_size = cfg["DATA"]["vocab_size"]
+
+        self.embedding = nn.Embedding(self.embed_size, self.embed_size)
+        self.pos_embeddings = nn.Embedding(self.embed_size, 1)
+        self.project = nn.Linear(self.embed_size, self.vocab_size)
 
     def forward(self, x):
-        out = x
+        x_emb = self.embedding(x)  # (B, T, embed_size)
+        pos_embedding = self.pos_embeddings(x)
+        out = out + pos_embedding
+        out = self.project(out)  # (B, T, C)
         return out
+
+    @torch.no_grad()
+    def generate(self, x, max_tokens):
+        for _ in range(max_tokens):
+            out = self(x[:, -1])  # (B, T, C)
+            out_prob = F.softmax(out, -1)
+            out_token = torch.multinomial(out_prob, 1).to(x.device)
+            x = torch.cat([x, out_token], -1)
+        return x
--- gpt/test/test_model.py
+++ gpt/test/test_model.py
@@ -8,12 +8,13 @@ import numpy as np
 import torch
 from dataset.tokenizer import WordLevelTokenizer
 from model.bigram_model import BigramModel
-from model.gpt_model import GPTModel
+from model.gpt_model import Attention, GPTModel
 from utils.misc import get_logger, load_config
 
 with tempfile.TemporaryDirectory() as tmp_dir:
     logger = get_logger(tmp_dir)
     cfg = load_config("config/gpt_config.yaml")
+    cfg["DATA"]["vocab_size"] = 65
 
 
 def test_gpt_model():
@@ -23,7 +24,6 @@ def test_gpt_model():
 
 def test_bigram_model():
     cfg_model = cfg["MODEL"]
-    cfg["DATA"]["vocab_size"] = 65
     model = BigramModel(cfg)
     B, T = 2, cfg_model["context_len"]
     x = torch.randint(0, cfg["MODEL"]["context_len"], (B, T))
@@ -45,6 +45,15 @@ def test_bigram_model_generation():
     max_tokens = 10
     out = model.generate(x_enc, max_tokens=max_tokens)
     assert out.shape == (B, x_enc.shape[1] + max_tokens)
+
+
+def test_attention():
+    embed_size = 32
+    B, T = 2, 10
+    x = torch.randn(B, T, embed_size)
+    module = Attention(embed_size=embed_size)
+    out = module(x)
+    assert out.shape == (B, T, embed_size)
 
 
 if __name__ == "__main__":
