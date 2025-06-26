8c7f80a24e3a5eb3c8880071b4ef3e3065905494
Aldi Piroli <aldipiroli@gmail.com>
2025-06-27 00:33:48 +0200

    fixed tests

--- gpt/test/test_dataset.py
+++ gpt/test/test_dataset.py
@@ -4,13 +4,13 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import tempfile
 
+from cfg import cfg
 from dataset.tiny_shakespeare import Tinyshakespeare
 from torch.utils.data import DataLoader
-from utils.misc import get_logger, load_config
+from utils.misc import get_logger
 
 with tempfile.TemporaryDirectory() as tmp_dir:
     logger = get_logger(tmp_dir)
-    cfg = load_config("config/gpt_config.yaml")
 
 
 def test_dataloader():
